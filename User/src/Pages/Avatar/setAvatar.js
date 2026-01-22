15cf854da0ae49cac5380b49edef8238091cbea9
komal <dethekomalv@gmail.com>
2026-01-22 15:47:41 +0530

    deployment error solving 2

--- User/src/Pages/Avatar/setAvatar.js
+++ User/src/Pages/Avatar/setAvatar.js
@@ -57,6 +57,7 @@ const SetAvatar = () => {
 
   const [selectedAvatar, setSelectedAvatar] = useState(undefined);
   const [loading, setLoading] = useState(false);
+  const [selectedSprite, setSelectedSprite] = useState(sprites[0]);
 
   useEffect(() => {
     if (!localStorage.getItem("user")) {
